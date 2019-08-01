3554e7332952b07d96d6ea7f4a6b6c5c59e45dff
zikrie <zikrie@DESKTOP-075KCFC>
2019-08-01 22:24:31 +0800

    new index cmrtw

--- resources/views/cmrtw/index.blade.php
+++ resources/views/cmrtw/index.blade.php
@@ -14,92 +14,19 @@
 <div class="row">
     <div class="col-md-12">
         <div class="card-body">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card text-left " style="float:right;width:500px;" >
-                            <div class="card-body" style="background-color: #98cb5b;">
-                                <table >
-                                    <thead>
-                                    <tr>
-                                        <th><label>Name</label></th>         
-                                        <th>:</th>
-                                        <th>@if(!empty($casertw)){{ $casertw->name }} @endif</th>
-                                    </tr>
-                                    <tr>
-                                        <th><label>ID No</label></th>
-                                        <th>:</th>
-                                        <th>@if(!empty($casertw)){{ $casertw->idno }} @endif</th>
-                                    </tr>
-                                    <tr>
-                                        <th><label>RTW Ref No</label></th>
-                                        <th>:</th>
-                                        <th></th>
-                                    </tr>
-                                    <tr>
-                                        <th><label>Receive Date/Submission Date</label></th>
-                                        <th>:</th>
-                                        <th></th>
-                                    </tr>
-                                    <tr>
-                                        <th><label>Source From</label></th>
-                                        <th>:</th>
-                                        <th></th>
-                                    </tr>
-                                    </thead>
-                                </table>
-                            </div>
-                    </div>
-                </div>
-            </div>
             <h4 class="card-title">Return To Work</h4>
-            <!-- <div class="row">
-            <div class="col-md-12">
-                <div class="card" >
-                    <div class="card-body">
-                        <div class="row p-t-20">
-                            <div class="col-md-3">
-                                <label class="control-label">@lang('registrationRtw.attr.name')</label>
-                                <input type="text" id="name" class="form-control" value="@if(!empty($casertw)){{ $casertw->name }} @endif" readonly>
-                            </div>
-                            <div class="col-md-3">
-                                <label for="example-text-input" class="control-label">@lang('offerconfirmation.attr.ictype')</label>
-                                <select class="form-control" value="@if(!empty($casertw)){{ $casertw->idtype }} @endif" disabled>
-                                    <option>Please Select</option>
-                                        @foreach($idtype as $id)
-                                        @if (!empty($casertw) && $id->refcode == $casertw->idtype)
-                                            <option value="{{$id->refcode}}" selected>{{$id->descen}}</option>
-                                        @else
-                                            <option value="{{$id->refcode}}">{{$id->descen}}</option>
-
-                                        @endif
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="col-md-3">
-                                <label for="example-text-input" class="control-label">@lang('offerconfirmation.attr.idno')</label>
-                                <input class="form-control" type="text"  id="idno" value="@if(!empty($casertw)){{ $casertw->idno }} @endif" readonly>
-                            </div>
-                            <div class="col-md-3">
-                                <label for="example-text-input" class="control-label">@lang('home.rtwrefno')</label>
-                                <input class="form-control" type="text"  id="idno" value="" readonly>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-            </div>
-            </div> -->
-
 
             <ul class="nav nav-tabs" role="tablist" >
                <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#registerrtw" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">@lang('index.attr.registration_application')</span></a> </li>
                <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#ia" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Initial Assessment</span></a></li>
            </ul>
            <!-- Nav tabs -->
-                <div class="tab-content tabcontent-border">
+                 <div class="tab-content tabcontent-border">
                     <div class="tab-pane p-20 active" id="registerrtw" role="tabpanel">
                              <ul class="nav customtab" role="tablist">
                     
-                                <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#registrationrtw" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Application Info</span></a></li>
+                                <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#registrationrtw" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Insured Person Info</span></a></li>
+                                <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#employerinfo" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Employer Info</span></a> </li>
                                 <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#supportingdocument" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">@lang('index.attr.supporting_document')</span></a> </li>
                                 <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#benefitCM" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Benefit Claim Info</span></a> </li>
                   
@@ -109,8 +36,11 @@
                                  <!-- RegistrationRTW details tab -->
                                  <div class="tab-pane p-20 active" id="registrationrtw" role="tabpanel">
                                    @include('cmrtw.rtwcase')
-                                   
                                 </div>
+
+                                <div class="tab-pane p-20" id="employerinfo" role="tabpanel">
+                                   @include('cmrtw.employerinfo')
+                               </div>
                                <!-- supporting details tab -->
                                <div class="tab-pane p-20" id="supportingdocument" role="tabpanel">
                                    @include('cmrtw.uploaddoc')
@@ -121,10 +51,60 @@
                            </div> 
                     </div>
                     <div class="tab-pane p-20" id="ia" role="tabpanel">
-                        @include('cmrtw.IA.try')          
+
+                        <ul class="nav customtab" role="tablist" id="calendarTab">
+
+                            <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#offerconfirmation" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Offer Confirmation</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#appointment" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Appointment</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#iarecovery" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Initial Assessment</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#rehabplan" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Rehab Plan</span></a> </li>
+
+
+                        </ul>
+
+                        <div class="tab-content tabcontent-border">
+
+                           <!-- RegistrationRTW details tab -->
+                           <div class="tab-pane p-20 active" id="offerconfirmation" role="tabpanel">
+                               @include('cmrtw.IA.offerconfirmation')
+                           </div>
+
+                           <!-- supporting details tab -->
+                           <div class="tab-pane p-20" id="appointment" role="tabpanel">
+                               @include('cmrtw.IA.appointment')
+                           </div>
+
+                           <div class="tab-pane p-20 " id="iarecovery" role="tabpanel">
+                              @include('cmrtw.IA.initialevaluation')
+                          </div>
+
+                           <div class="tab-pane p-20 " id="initialevaluation" role="tabpanel">
+                                <ul class="nav nav-tabs customtab2" role="tablist">
+
+                                <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#iarecovery" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Initial Assessment Recovery</span></a> </li>
+                                <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#supdoc" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">@lang('index.attr.supporting_document')</span></a> </li>
+
+                                </ul>
+                                <div class="tab-content tabcontent-border">
+
+                               <!-- RegistrationRTW details tab -->
+                                     <div class="tab-pane p-20 active" id="iarecovery" role="tabpanel">
+                                          @include('cmrtw.IA.initialevaluation')
+                                    </div>
+
+                               <!-- supporting details tab -->
+                                    <div class="tab-pane p-20" id="supdoc" role="tabpanel">
+                                      @include('cmrtw.IA.appointment')
+                                    </div>
+                                 </div>
+                            </div>
+
+                             <div class="tab-pane p-20 " id="rehabplan" role="tabpanel">
+                                     @include('cmrtw.IA.rehabplan')
+                             </div>
+                        </div>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
 </div>
@@ -142,11 +122,6 @@
         });
     });
 
-
-    //redirect to specific tab
-    $(document).ready(function () {
-    $('#tabMenu a[href="#{{ old('tab') }}"]').tab('show')
-    });
 </script>
 @endsection
 
